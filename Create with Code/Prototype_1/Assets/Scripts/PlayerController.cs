da6ca68254154806ec9bb534c07ac3dcadd3b19d
SHyguymoll <shymollet@gmail.com>
2023-12-07 13:18:39 -0800

    Lesson 1.4 - 3

--- Create with Code/Prototype_1/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype_1/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 20.5F;
     public float turnSpeed;
     public float horiInp;
+    public float vertInp;
     
     // Start is called before the first frame update
     void Start()
@@ -18,11 +19,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horiInp = Input.GetAxis("Horizontal");
+        vertInp = Input.GetAxis("Vertical");
         // look ma, no hands!
         /*
             - moments before crashing the car
         */
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertInp);
         // look ma, hands!
         /*
             - moments before veering the car off of a cliff
