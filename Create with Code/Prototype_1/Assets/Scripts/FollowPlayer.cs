341e138c8b335d7f35aef1a7fb0f7df56eaf61c1
SHyguymoll <shymollet@gmail.com>
2023-12-07 12:56:59 -0800

    Lesson 1.3 - 2
    I don't even need to test this, I know that it will send the camera to the green zone

---
+++ Create with Code/Prototype_1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject cameraTarget;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraTarget.transform.position;
+    }
+}
